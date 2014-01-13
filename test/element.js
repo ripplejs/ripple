937c5dcfbbb66222cd6796e2d0a0112f0b645e1e
Anthony Short <antshort@gmail.com>
2014-01-13 14:27:03 -0800

    Doc updates

---
+++ test/element.js
@@ -0,0 +1,13 @@
+var stream = require('ripple').stream;
+
+describe('element stream', function(){
+
+  describe('attribute stream', function(){
+    var el = document.createElement('div');
+  })
+
+  describe('html stream', function(){
+
+  })
+
+})
