daeb73f55ec646f8b2f6222a39bd910cb5903a8f
Anthony Short <antshort@gmail.com>
2014-02-15 21:32:44 -0800

    Using updated view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Compiler = require('compiler');
 var view = require('view');
+var interpolate = require('interpolate');
 
 /**
  * Returns a function that creates a view
@@ -8,24 +9,108 @@ var view = require('view');
  * @type {Function}
  */
 module.exports = function(template) {
-  if(template.nodeType) template = template.outerHTML;
-
   var compiler = new Compiler();
   var View = view(template);
+  var filters = {};
 
-  View.component = function(name, fn) {
-    compiler.addComponent(name, fn);
+  /**
+   * Add a component
+   *
+   * @param {String} match
+   * @param {Function} fn
+   *
+   * @return {View}
+   */
+  View.component = function(match, fn) {
+    compiler.addComponent(match, fn);
     return this;
   };
 
-  View.directive = function(name, fn) {
-    compiler.addAttribute(name, fn);
+  /**
+   * Add a directive
+   *
+   * @param {String|Regex} match
+   * @param {Function} fn
+   *
+   * @return {View}
+   */
+  View.directive = function(match, fn) {
+    compiler.addAttribute(match, fn);
     return this;
   };
 
-  View.render(function(view){
-    compiler.compile(view);
-  });
+  /**
+   * Add an interpolation filter
+   *
+   * @param {String} name
+   * @param {Function} fn
+   *
+   * @return {View}
+   */
+  View.filter = function(name, fn) {
+    filters[name] = fn;
+    return this;
+  };
+
+  /**
+   * Run an interpolation on the string using the state. Whenever
+   * the model changes it will render the string again
+   *
+   * @param {String} str
+   * @param {Function} callback
+   *
+   * @return {Function} a function to unbind the interpolation
+   */
+  View.prototype.interpolate = function(str, callback) {
+    var self = this;
+    var attrs = interpolate.props(str);
+    if(attrs.length === 0) return;
+    function render() {
+      return interpolate(str, self.get(attrs), filters);
+    }
+    if(!callback) return render();
+    callback(render());
+    return this.change(attrs, function(){
+      callback(render());
+    });
+  };
+
+  /**
+   * Causes all bindings that are watching
+   * this view to attach themselves
+   *
+   * @return {View}
+   */
+  View.prototype.bind = function(){
+    if(this.bound) return this;
+    this.bound = true;
+    this.emit('bind', this);
+    return this;
+  };
+
+  /**
+   * Unbind all events and binding from the view,
+   * leaving just the element in place
+   * @return {View}
+   */
+  View.prototype.unbind = function(){
+    if(!this.bound) return this;
+    this.bound = false;
+    this.emit('unbind', this);
+    return this;
+  };
+
+  /**
+   * When the view renders the compiler
+   * will run over element and bind it
+   *
+   * @param {View} view
+   *
+   * @return {void}
+   */
+  View.prototype.render = function(){
+    compiler.compile(this);
+  };
 
   return View;
 };
--- test/tests.js
+++ test/tests.js
@@ -16,13 +16,6 @@ describe('Ripple', function(){
     assert(view.state);
   });
 
-  it('should accept elements instead of a template', function(){
-    var div = document.createElement('div');
-    var View = ripple(div);
-    var view = new View();
-    assert(view.el.nodeName === 'DIV');
-  });
-
   it('should add directives', function(){
     View = ripple('<div data-text="name"></div>');
     View.directive('data-text', function(view, el, attr, value){
@@ -32,6 +25,84 @@ describe('Ripple', function(){
       name: 'Tom'
     });
     assert(view.el.innerHTML === "Tom");
+  });
+
+  describe('interpolation', function () {
+
+    beforeEach(function () {
+      View = ripple('<div></div>');
+    })
+
+    it('should interpolate a string', function(done){
+      var view = new View();
+      view.set('foo', 'bar');
+      view.interpolate('{{foo}}', function(val){
+        assert(val === 'bar');
+        done();
+      });
+    })
+
+    it('should add interpolation filters', function(done){
+      View.filter('noobify', function(){
+        return 'noob';
+      });
+      var view = new View();
+      view.set('foo', 'bar');
+      view.interpolate('{{foo | noobify}}', function(val){
+        assert(val === 'noob');
+        done();
+      });
+    })
+
+   it('should watch a string for changes', function(done){
+      var count = 0;
+      var view = new View();
+      view.set('foo', 'bar');
+      view.interpolate('{{foo}}', function(val){
+        count++;
+        if(count === 1) assert(val === 'bar');
+        if(count === 2) {
+          assert(val === 'baz');
+          done();
+        }
+      });
+      view.set('foo', 'baz');
+    })
+
+    it('should unwatch a strings changes', function(){
+      var count = 0;
+      var view = new View();
+      view.set('foo', 'bar');
+      var unbind = view.interpolate('{{foo}}', function(val){
+        count++;
+      });
+      unbind();
+      view.set('foo', 'baz');
+      assert(count === 1, count);
+    })
+
+  });
+
+  describe('binding events', function () {
+
+    it('should bind and fire an event', function(done){
+      var view = new View();
+      view.on('bind', function(){
+        done();
+      });
+      view.bind();
+    })
+
+    it('should unbind and fire an event', function(done){
+      var view = new View();
+      view.on('unbind', function(){
+        done();
+      });
+      view
+        .bind()
+        .unbind();
+    })
+
   });
 
 })
