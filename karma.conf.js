344d4d66a480841ffcfb4d93ee90fb37157f24a6
Anthony Short <antshort@gmail.com>
2014-03-21 11:25:42 -0700

    Cleaned up karma file

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,23 @@
-// Karma configuration
-// Generated on Fri Mar 21 2014 11:02:39 GMT-0700 (PDT)
-
 module.exports = function(config) {
   config.set({
-
-    // base path, that will be used to resolve files and exclude
     basePath: '',
-
-
-    // frameworks to use
+    autoWatch: false,
+    port: 9876,
+    colors: true,
+    captureTimeout: 60000,
+    singleRun: true,
+    logLevel: config.LOG_INFO,
     frameworks: ['mocha'],
-
-
-    // list of files / patterns to load in the browser
+    reporters: ['progress'],
     files: [
       'build/build.js',
       'test/lib/**/*.js'
     ],
-
-
-    // list of files to exclude
-    exclude: [
-
-    ],
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
     browsers: [
       'Chrome',
       'Firefox',
       'Safari',
       'PhantomJS'
-    ],
-
-
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
-
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: true
+    ]
   });
 };
