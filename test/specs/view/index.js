33ce26a398f438ad3e989e4b4492a20e96e5dd9f
Anthony Short <antshort@gmail.com>
2014-03-21 17:02:47 -0700

    Removed computed properties and owner

--- test/specs/view/index.js
+++ test/specs/view/index.js
@@ -36,17 +36,6 @@ describe('View', function(){
       assert( view.get('one') === 'prop' );
     });
 
-    it('should get state from the owner', function () {
-      var parent = new View();
-      parent.state.set('one', 'one');
-      var child = new View(null, {
-        owner: parent
-      });
-      child.state.set('foo', 'bar');
-      assert( child.get('foo') === 'bar' );
-      assert( child.get('one') === 'one' );
-    });
-
   });
 
   describe('state', function () {
@@ -70,47 +59,13 @@ describe('View', function(){
       assert(view.state.foo === 'bar');
     });
 
-    it('should have computed properties', function(){
-      View.computed('fullname', ['firstname', 'lastname'], function(first, last){
-        return [first,last].join(' ');
-      });
-      var view = new View();
-      view.set({
-        'firstname': 'Bruce',
-        'lastname': 'Willis'
-      });
-      assert(view.state.get('fullname') === 'Bruce Willis');
-      view.state.set('firstname', 'Danny');
-      assert(view.state.get('fullname') === 'Danny Willis');
-    })
-
-    it('should have computed properties with nested dependencies', function(){
-      View.computed('fullname', ['names.first', 'names.last'], function(first, last){
-        return [first,last].join(' ');
-      });
-      var view = new View();
-      view.set({
-        names: {
-          first: 'Bruce',
-          last: 'Willis'
-        }
-      });
-      assert(view.get('fullname') === 'Bruce Willis');
-      view.set({
-        names: {
-          first: 'Jet',
-          last: 'Li'
-        }
-      });
-      assert(view.get('fullname') === 'Jet Li');
-    })
-
     it('should watch for changes', function(done){
       var view = new View();
+      view.set('foo', 'bar');
       view.change('foo', function(){
         done();
       })
-      view.set('foo', 'bar');
+      view.set('foo', 'baz');
     })
 
     it('should be able to set default properties', function () {
@@ -137,14 +92,6 @@ describe('View', function(){
       assert(view.props.get('foo') === 'bar');
     });
 
-    it('should not allow setting new props', function(){
-      var view = new View({
-        foo: 'bar'
-      });
-      view.props.set('bar', 'foo');
-      assert( view.props.get('bar') === undefined )
-    })
-
   });
 
   describe('having an owner', function () {
@@ -157,14 +104,6 @@ describe('View', function(){
       assert(child.root == parent);
       assert(grandchild.owner == child);
       assert(grandchild.root == parent);
-    });
-
-    it('should get state from the owner', function () {
-      var parent = new View();
-      parent.state.set('foo', 'bar');
-      var child = new View(null, { owner: parent });
-      var grandchild = new View(null, { owner: child });
-      assert(grandchild.get('foo') === 'bar');
     });
 
   });
