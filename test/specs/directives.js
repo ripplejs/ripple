7f095d02894556203b7ddf71968109ab705bc413
Anthony Short <antshort@gmail.com>
2014-03-27 09:30:01 -0700

    Added directive as a binding

--- test/specs/directives.js
+++ test/specs/directives.js
@@ -4,26 +4,53 @@ describe('directives', function () {
 
   it('should match directives with a string', function(done){
     var View = ripple('<div data-test="foo"></div>');
-    View.directive('data-test', function(view, el, value){
+    View.directive('data-test', {
+      update: function(value){
+        assert(value === 'foo');
+        done();
+      }
+    });
+    var view = new View();
+    view.appendTo('body');
+    view.destroy();
+  });
+
+  it('should use just an update method', function(done){
+    var View = ripple('<div data-test="foo"></div>');
+    View.directive('data-test', function(value){
       assert(value === 'foo');
       done();
     });
     var view = new View();
-    view.appendTo('body');
-    view.remove();
   });
 
-  it('should not interpolate directive values', function(done){
+  it('should update with interpolated values', function(done){
     var View = ripple('<div data-test="{{foo}}"></div>');
-    View.directive('data-test', function(view, el, value){
-      assert(value === '{{foo}}');
-      done();
+    View.directive('data-test', {
+      update: function(value) {
+        assert(value === 'bar');
+        done();
+      }
     });
     var view = new View({
-      foo: 'bar'
+      data: {
+        foo: 'bar'
+      }
     });
-    view.appendTo('body');
-    view.remove();
+  });
+
+  it('should call the binding in the context of the directive', function () {
+    var view;
+    var View = ripple('<div data-test="foo"></div>');
+    View.directive('data-test', function(value){
+      assert(this.node === view.el);
+      assert(this.view === view);
+    });
+    view = new View();
+  });
+
+  it.skip('should not run job if the view is destroyed', function(){
+
   });
 
 });
