bcc1e0508fba7a2deb8562c89d1ab565e01168cc
chrisbuttery <info@chrisbuttery.com>
2014-04-23 21:05:14 +1000

    Updated markdown-editor and timer examples

--- examples/markdown-editor/index.js
+++ examples/markdown-editor/index.js
@@ -1,13 +1,17 @@
 var ripple = require('ripple');
-var template = require('template');
+var template = require('./template.html');
 var events = require('events');
 var markdown = require('markdown');
 var refs = require('refs');
 
 var MarkdownEditor = ripple(template)
-  .use(events)
   .use(markdown)
+  .use(events)
   .use(refs);
+
+MarkdownEditor.created(function(){
+  this.set('text', '');
+});
 
 // This is fired whenever the inputs "input" event is fired
 MarkdownEditor.prototype.change = function() {
