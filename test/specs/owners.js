110bc8ad0befee9e3270eb175629e58e40d57fda
Anthony Short <antshort@gmail.com>
2014-06-11 13:45:57 -0700

    Split out the proto and static methods

--- test/specs/owners.js
+++ test/specs/owners.js
@@ -17,8 +17,8 @@ describe('owners', function () {
   });
 
   it('should be able to have an owner', function () {
-    assert(child.owner === parent);
-    assert(grandchild.owner == child);
+    assert(child.owner() === parent);
+    assert(grandchild.owner() == child);
   });
 
   it('should set the root', function () {
